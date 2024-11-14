616c0cb249388fcc3a4b4c09ec22be141938d489
Thomas Nishiduka <thomas.nishiduka@gmail.com>
2024-11-13 23:31:39 -0300

    chore: adicionado PRD

--- src/main/java/dev/nishiduka/cae/telao/core/repository/AgendamentoRecorrenteRepository.java
+++ src/main/java/dev/nishiduka/cae/telao/core/repository/AgendamentoRecorrenteRepository.java
@@ -3,20 +3,22 @@ package dev.nishiduka.cae.telao.core.repository;
 import dev.nishiduka.cae.telao.core.domain.dtos.AgendamentoRecorrenteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface AgendamentoRecorrenteRepository extends JpaRepository<AgendamentoRecorrenteEntity, Long> {
-    @Query("SELECT CASE WHEN COUNT (a) > 0 THEN true ELSE false END FROM AgendamentoRecorrenteDTO a WHERE a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
+    @Query("SELECT CASE WHEN COUNT (a) > 0 THEN true ELSE false END FROM AgendamentoRecorrenteEntity a WHERE a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
     boolean validarConflitoAgenda(AgendamentoRecorrenteEntity agendamento);
 
-    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM AgendamentoRecorrenteDTO a WHERE a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM AgendamentoRecorrenteEntity a WHERE a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
     boolean validarConflitoAgendaIgnorandoID(AgendamentoRecorrenteEntity agendamento);
 
-    @Query("FROM AgendamentoRecorrenteDTO a WHERE a.id != :#{#agendamento.id} AND a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
+    @Query("FROM AgendamentoRecorrenteEntity a WHERE a.id != :#{#agendamento.id} AND a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
     List<AgendamentoRecorrenteEntity> buscarAgendamentoPorSalaHorario(AgendamentoRecorrenteEntity agendamento);
 
-    List<AgendamentoRecorrenteEntity> findBySalaId(Long salaId);
+    @Query("FROM AgendamentoRecorrenteEntity a WHERE a.sala.id = :salaId")
+    List<AgendamentoRecorrenteEntity> findBySalaId(@Param("salaId") Long salaId);
 }
