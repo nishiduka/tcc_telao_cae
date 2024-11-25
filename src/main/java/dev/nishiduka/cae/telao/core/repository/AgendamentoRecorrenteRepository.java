a1ddd31b46b747760590f0b1b4fae0dba8d334b7
Thomas Nishiduka <thomas.nishiduka@gmail.com>
2024-11-24 23:47:13 -0300

    feat: atualizado query

--- src/main/java/dev/nishiduka/cae/telao/core/repository/AgendamentoRecorrenteRepository.java
+++ src/main/java/dev/nishiduka/cae/telao/core/repository/AgendamentoRecorrenteRepository.java
@@ -1,10 +1,13 @@
 package dev.nishiduka.cae.telao.core.repository;
 
 import dev.nishiduka.cae.telao.core.domain.dtos.AgendamentoRecorrenteEntity;
+import dev.nishiduka.cae.telao.core.domain.enums.EDiaSemana;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -17,4 +20,9 @@ public interface AgendamentoRecorrenteRepository extends JpaRepository<Agendamen
 
     @Query("FROM AgendamentoRecorrenteEntity a WHERE a.id != :#{#agendamento.id} AND a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
     List<AgendamentoRecorrenteEntity> buscarAgendamentoPorSalaHorario(AgendamentoRecorrenteEntity agendamento);
+
+    @Query("FROM AgendamentoRecorrenteEntity a WHERE a.sala.id = :salaId")
+    List<AgendamentoRecorrenteEntity> findBySalaId(@Param("salaId") Long salaId);
+
+    List<AgendamentoRecorrenteEntity> findByDiaSemana(EDiaSemana diaExtenso);
 }
