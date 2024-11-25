7fa652b634918789d0fc55a842ab79c6a51b0c78
Thomas Nishiduka <thomas.nishiduka@gmail.com>
2024-11-24 23:53:57 -0300

    feat: atualizado query

--- src/main/java/dev/nishiduka/cae/telao/core/repository/AgendamentoRecorrenteRepository.java
+++ src/main/java/dev/nishiduka/cae/telao/core/repository/AgendamentoRecorrenteRepository.java
@@ -15,7 +15,7 @@ public interface AgendamentoRecorrenteRepository extends JpaRepository<Agendamen
     @Query("SELECT CASE WHEN COUNT (a) > 0 THEN true ELSE false END FROM AgendamentoRecorrenteEntity a WHERE a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
     boolean validarConflitoAgenda(AgendamentoRecorrenteEntity agendamento);
 
-    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM AgendamentoRecorrenteEntity a WHERE a.id != :#{#a.id} AND a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM AgendamentoRecorrenteEntity a WHERE a.id != :#{#agendamento.id} AND a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
     boolean validarConflitoAgendaIgnorandoID(AgendamentoRecorrenteEntity agendamento);
 
     @Query("FROM AgendamentoRecorrenteEntity a WHERE a.id != :#{#agendamento.id} AND a.diaSemana = :#{#agendamento.diaSemana} AND a.sala.id = :#{#agendamento.sala.id} AND ((a.horaInicio < :#{#agendamento.horaFim} AND a.horaFim > :#{#agendamento.horaInicio}))")
